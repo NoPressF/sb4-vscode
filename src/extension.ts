d83fc7aa6e6a1864d4fd159a8ff0f3d09b6398d4
Vladimir <68227685+NoPressF@users.noreply.github.com>
2025-03-06 20:45:41 +0100

    Added selectFolder to set path Sanny Builder 4 folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sb4" is now active!');
+    let disposable = vscode.commands.registerCommand('sb4.selectFolder', async () => {
+        const folderUri = await vscode.window.showOpenDialog({
+            canSelectFiles: false,
+            canSelectFolders: true,
+            canSelectMany: false,
+            openLabel: 'Select SB4 Folder'
+        });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('sb4.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Sanny Builder 4!');
-	});
+        if (folderUri && folderUri[0]) {
+            const folderPath = folderUri[0].fsPath;
 
-	context.subscriptions.push(disposable);
+            context.globalState.update('selectedFolderPath', folderPath);
+
+            vscode.window.showInformationMessage(`Path to SB4 folder was successfully selected.`);
+        }
+    });
+
+    context.subscriptions.push(disposable);
+
+    const savedFolderPath = context.globalState.get('selectedFolderPath');
+    if (savedFolderPath) {
+        vscode.window.showInformationMessage(`Previously selected folder: ${savedFolderPath}`);
+    }
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
