433f41c9ae0afbd45f2383d3292c46ce146cbed5
Vladimir <68227685+NoPressF@users.noreply.github.com>
2025-03-10 21:49:31 +0100

    Added syntax the highlight enums & Added error line number if compilation failed

--- src/compile-script.ts
+++ src/compile-script.ts
@@ -97,9 +97,22 @@ export const CompileScript = {
 function createFileLevelDiagnostics(errorMessage: string): vscode.Diagnostic[] {
     const diagnostics: vscode.Diagnostic[] = [];
 
-    const range = new vscode.Range(0, 0, 0, 0);
-    const diagnostic = new vscode.Diagnostic(range, errorMessage, vscode.DiagnosticSeverity.Error);
+    const regex = /error:\s(.+):(\d+)\s(.*)/g;
+    const matches = [...errorMessage.matchAll(regex)];
 
-    diagnostics.push(diagnostic);
+    if (matches.length > 0) {
+        const [, file, lineStr, message] = matches[0];
+        const lineNumber = parseInt(lineStr, 10);
+
+        const range = new vscode.Range(lineNumber, 0, lineNumber, Number.MAX_SAFE_INTEGER);
+
+        const diagnostic = new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error);
+        diagnostics.push(diagnostic);
+    } else {
+        const range = new vscode.Range(0, 0, 0, 0);
+        const diagnostic = new vscode.Diagnostic(range, errorMessage, vscode.DiagnosticSeverity.Error);
+        diagnostics.push(diagnostic);
+    }
+
     return diagnostics;
 }
--- src/language.ts
+++ src/language.ts
@@ -7,14 +7,14 @@ import { GTA_VERSIONS } from 'gta-version';
 export const Language = {
     async applyColors(context: vscode.ExtensionContext): Promise<void> {
         const colorsPath = path.join(context.extensionPath, 'syntaxes', 'sb4.tmColors.json');
-    
+
         const colorData = fs.readFileSync(colorsPath, 'utf8');
         const colors = JSON.parse(colorData);
-    
+
         if (!colors || !colors.textMateRules) {
             return;
         }
-    
+
         const config = vscode.workspace.getConfiguration();
         await config.update('editor.tokenColorCustomizations', colors, vscode.ConfigurationTarget.Global);
     },
@@ -28,59 +28,75 @@ export const Language = {
                 "name": "SB",
                 "patterns": []
             };
-            
+
             fs.writeFileSync(grammarPath, JSON.stringify(baseStructure, null, 4), 'utf8');
         }
 
         const grammarData = fs.readFileSync(grammarPath, 'utf8');
         const grammarJson = JSON.parse(grammarData);
-    
+
         const sbFolderPath = context.globalState.get('selectedSB4FolderPath') as string;
-    
+
         if (sbFolderPath === undefined) {
             Folder.showSB4FolderSelectionPrompt();
             return;
         }
-    
+
         grammarJson.patterns = [];
-    
+
         const selectedGtaVersion = context.globalState.get('selectedGtaVersion');
         const gtaVersionData = GTA_VERSIONS.find(version => version.label === selectedGtaVersion);
 
+        if (!gtaVersionData) {
+            vscode.window.showErrorMessage('Selected GTA version not found.');
+            return;
+        }
+
         const functionNamesFilePath = path.join(
-            sbFolderPath, 
-            'data', 
-            gtaVersionData?.identifier as string, 
-            gtaVersionData?.functionsFile as string
+            sbFolderPath,
+            'data',
+            gtaVersionData.identifier,
+            gtaVersionData.functionsFile
         );
 
         const functionNamesJson = fs.readFileSync(functionNamesFilePath, 'utf8');
         const functionNamesList = JSON.parse(functionNamesJson);
-    
+
+        const enumsFilePath = path.join(
+            sbFolderPath,
+            'data',
+            gtaVersionData.identifier,
+            'enums.txt'
+        );
+
+        const enumNames = fs.readFileSync(enumsFilePath, 'utf8');
+        const enumRegex = /(?<=enum\s)(\w+)/g;
+
         const functions = [];
         const classes = [];
         const methods = [];
-    
+        const enums = [...enumNames.matchAll(enumRegex)].map(match => match[1]);
+
         for (const extension of functionNamesList.extensions) {
             for (const command of extension.commands) {
                 const commandClass = command.class;
                 const memberClass = command.member;
-    
+
                 if (commandClass === undefined && memberClass === undefined) {
                     functions.push(command.name.toLowerCase());
                     continue;
                 }
-    
+
                 if (commandClass !== undefined) {
                     classes.push(commandClass);
                 }
-    
+
                 if (memberClass !== undefined) {
                     methods.push(memberClass);
                 }
             }
         }
-    
+
         if (functions.length > 0) {
             const functionPattern = {
                 "match": `(?i)\\b(${functions.join('|')})\\b`,
@@ -88,7 +104,7 @@ export const Language = {
             };
             grammarJson.patterns.push(functionPattern);
         }
-    
+
         if (classes.length > 0) {
             const classPattern = {
                 "match": `\\b(${classes.join('|')})\\b`,
@@ -96,7 +112,7 @@ export const Language = {
             };
             grammarJson.patterns.push(classPattern);
         }
-    
+
         if (methods.length > 0) {
             const methodPattern = {
                 "match": `\\b(${methods.join('|')})\\b`,
@@ -104,7 +120,15 @@ export const Language = {
             };
             grammarJson.patterns.push(methodPattern);
         }
-    
+
+        if (enums.length > 0) {
+            const enumPattern = {
+                match: `\\b(${enums.join('|')})\\b`,
+                name: 'enum.sb'
+            };
+            grammarJson.patterns.push(enumPattern);
+        }
+
         fs.writeFileSync(grammarPath, JSON.stringify(grammarJson, null, 4), 'utf8');
     }
 };
